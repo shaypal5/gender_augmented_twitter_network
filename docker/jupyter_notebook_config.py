311ab54fbdca2bf5807eaf87d9edbe3e3c9c2dc2
Shay Palachy <shaypal5@gmail.com>
2019-03-19 17:23:13 +0200

    docker getting better - notboke working

---
+++ docker/jupyter_notebook_config.py
@@ -0,0 +1,2 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.allow_root =  True
--- docker/run.sh
+++ docker/run.sh
@@ -1,1 +1,2 @@
+# docker run -p 8888:8888 -p 6006:6006 -it -u user -w /home/user shaypal5/twikwak17:latest bash
 docker-compose -f "shay_ubuntu_compose.yml" run twikwak17
